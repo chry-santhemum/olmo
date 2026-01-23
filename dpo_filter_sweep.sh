23b2b0d2e75023fb50a1fee5f30f047c18dde4cd
chry-santhemum <zifanawang04@gmail.com>
2026-01-23 19:08:47 +0000

    change num processes for sweep

--- dpo_filter_sweep.sh
+++ dpo_filter_sweep.sh
@@ -76,7 +76,7 @@ train_dpo() {
     accelerate launch \
         --mixed_precision bf16 \
         --num_machines 1 \
-        --num_processes 4 \
+        --num_processes 8 \
         --use_deepspeed \
         --deepspeed_config_file configs/ds_configs/stage3_no_offloading_accelerate.conf \
         open_instruct/dpo_tune_cache.py \
@@ -87,7 +87,7 @@ train_dpo() {
         --tokenizer_name="allenai/Olmo-3-7B-Instruct-SFT" \
         --max_seq_length=16384 \
         --per_device_train_batch_size=1 \
-        --gradient_accumulation_steps=32 \
+        --gradient_accumulation_steps=16 \
         --learning_rate=1e-6 \
         --lr_scheduler_type=linear \
         --warmup_ratio=0.1 \
